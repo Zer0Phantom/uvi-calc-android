68c4feffdf4f75d6266bf4721ba719c6ce5ab502
Georg Baumann <baumanngeorg5@gmail.com>
2020-06-09 18:34:02 +0200

    cleanup

--- app/src/main/java/de/baumanngeorg/uvilsfrechner/datasource/dwd/model/DwdUviContent.kt
+++
@@ -1,6 +0,0 @@
-package de.baumanngeorg.uvilsfrechner.datasource.dwd.model
-
-data class DwdUviContent(
-        val city: String = "Berlin",
-        val forecast: DwdUviForecast = DwdUviForecast()
-)
--- app/src/main/java/de/baumanngeorg/uvilsfrechner/datasource/dwd/model/DwdUviForecast.kt
+++
@@ -1,7 +0,0 @@
-package de.baumanngeorg.uvilsfrechner.datasource.dwd.model
-
-data class DwdUviForecast(
-        val today: Int = 8,
-        val tomorrow: Int = 8,
-        val dayafter_to: Int = 8
-)
